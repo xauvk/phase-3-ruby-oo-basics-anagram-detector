e280fa9aca95d19d00e138223139ff054831e18f
xauvk <xauvkubalexanderyang@live.com>
2022-08-08 17:17:31 -0400

    commit tree c0b6a2c034945ab94e6efa9905982a156c5435af

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        wordArray = @word.chars
+        matches = array.filter do |a|
+            aChar = a.chars
+            wordArray.sort == aChar.sort
+        end
+        matches
+    end
+end
